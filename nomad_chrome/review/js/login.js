5a6301dfbe9b06fe979c17fe529643821c4d2537
nang-e <yeanvely@naver.com>
2022-04-28 22:49:50 +0900

    2022-04-28

---
+++ nomad_chrome/review/js/login.js
@@ -0,0 +1,11 @@
+const userInput = document.querySelector('.login input')
+const USERNAME = userInput.value
+const HIDDENCLASS = 'hidden'
+
+document.querySelector('.login h2').classList.add(HIDDENCLASS)
+// function loginfunc(){
+//   if(localStorage.getItem(USERNAME)){
+
+//   }
+// }
+// document.addEventListener('submit',loginfunc)
